2b720a0ee3f163d6b442dfb4b962245f4a662ab0
Jerrod Junker <unsafepond@gmail.com>
2015-12-14 21:44:23 -0700

    changed the welcome index a bit

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "/auth/twitch", as: "login"
 
   get "/auth/twitch/callback", to: "sessions#create"
+
+  get "/logout", to: "sessions#destroy", as: "logout"
 end
